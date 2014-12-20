51b5d6e39b0825689ded72908ef0e50bc91f7dab
toshihirock <toshihiro308@gmail.com>
2014-12-21 07:40:29 +0900

    support JUnit4 style

--- src/test/java/com/example/espresso/activity/DeckardEspressoTest.java
+++ src/test/java/com/example/espresso/activity/DeckardEspressoTest.java
@@ -5,11 +5,21 @@ import android.test.suitebuilder.annotation.LargeTest;
 import com.example.R;
 import com.example.robolectric.DeckardActivity;
 
-import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
-import static com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions.matches;
-import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
-import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withText;
+//import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.onView;
+//import static com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+//import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+//import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withText;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.InstrumentationRegistry;
 
+@RunWith(AndroidJUnit4.class)
 @LargeTest
 public class DeckardEspressoTest extends ActivityInstrumentationTestCase2<DeckardActivity> {
 
@@ -18,13 +28,15 @@ public class DeckardEspressoTest extends ActivityInstrumentationTestCase2<Deckar
        // This constructor was deprecated - but we want to support lower API levels.
        super("com.example.activity", DeckardActivity.class);
      }
-    @Override
+    @Before
     public void setUp() throws Exception {
         super.setUp();
         // Espresso will not launch our activity for us, we must launch it via getActivity().
+        injectInstrumentation(InstrumentationRegistry.getInstrumentation());
         getActivity();
     }
 
+    @Test
     public void testCheckText() {
         onView(withId(R.id.text))
             .check(matches(withText("Hello Espresso!")));
