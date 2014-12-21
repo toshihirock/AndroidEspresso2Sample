ef8e2e5097d32d77bb0a00dd49d651c30ce7eeee
toshihirock <toshihiro308@gmail.com>
2014-12-21 10:15:17 +0900

    add after annotation

--- src/test/java/com/example/espresso/activity/DeckardEspressoTest.java
+++ src/test/java/com/example/espresso/activity/DeckardEspressoTest.java
@@ -5,15 +5,12 @@ import android.test.suitebuilder.annotation.LargeTest;
 import com.example.R;
 import com.example.robolectric.DeckardActivity;
 
-//import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView;
 import static android.support.test.espresso.Espresso.onView;
-//import static com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-//import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-//import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withText;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import org.junit.Before;
+import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import android.support.test.runner.AndroidJUnit4;
@@ -40,5 +37,10 @@ public class DeckardEspressoTest extends ActivityInstrumentationTestCase2<Deckar
     public void testCheckText() {
         onView(withId(R.id.text))
             .check(matches(withText("Hello Espresso!")));
+      }
+
+      @After
+      public void tearDown() throws Exception {
+          super.tearDown();
       }
 }
